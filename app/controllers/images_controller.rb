38aecfe02fbaba1bed81b74e3133390f066afe66
Ryan Wick <ryan.wick@oregonstate.edu>
2015-10-24 00:15:24 -0700

    Expanded images controller and views.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,13 +8,38 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.new
+  end
+
+  def edit
+    @image = Image.find(params[:id])
   end
 
   def create
     @image = Image.new(image_params)
  
-    @image.save
-    redirect_to @image
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @image = Image.find(params[:id])
+  
+    if @image.update(image_params)
+      redirect_to @image
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+  
+    redirect_to images_path
   end
 
   private
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,4 @@
 class Image < ActiveRecord::Base
+  validates :title, presence: true
+
 end
