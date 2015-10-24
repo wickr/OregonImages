956cb3fd5d61e1c5b99178d4f67c1480e2ce0a52
Ryan Wick <ryan.wick@oregonstate.edu>
2015-10-23 23:30:57 -0700

    Added Image model and migrations. Added basic views.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,24 @@
 class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @image = Image.new(image_params)
+ 
+    @image.save
+    redirect_to @image
+  end
+
+  private
+    def image_params
+      params.require(:image).permit(:title, :description)
+    end
 end
