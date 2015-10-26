fda87bfeaf6923094fe1d4b70b01b3ef69bb42ca
Ryan Wick <ryan.wick@oregonstate.edu>
2015-10-25 18:34:20 -0700

    Edit cultural properties controller and set up views.

--- app/controllers/cultural_properties_controller.rb
+++ app/controllers/cultural_properties_controller.rb
@@ -1,2 +1,49 @@
 class CulturalPropertiesController < ApplicationController
+  def index
+    @cps = CulturalProperty.all
+  end
+
+  def show
+    @cp = CulturalProperty.find(params[:id])
+  end
+
+  def new
+    @cp = CulturalProperty.new
+  end
+
+  def edit
+    @cp = CulturalProperty.find(params[:id])
+  end
+
+  def create
+    @cp = CulturalProperty.new(cp_params)
+ 
+    if @cp.save
+      redirect_to @cp
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @cp = CulturalProperty.find(params[:id])
+  
+    if @cp.update(cp_params)
+      redirect_to @cp
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @cp = CulturalProperty.find(params[:id])
+    @cp.destroy
+  
+    redirect_to cps_path
+  end
+
+  private
+    def cp_params
+      params.require(:cp).permit(:title, :description)
+    end
 end
